d8d1ef2cb967649344c38184e3ea76feed4af095
lstarlitsky <liu.shunming@foxmail.com>
2021-07-22 20:25:45 +0800

    双向链表demo

--- src/com/mit/linkedlist/DoubleLinkedListDemo.java
+++ src/com/mit/linkedlist/DoubleLinkedListDemo.java
@@ -1,75 +1,89 @@
 package com.mit.linkedlist;
 
-import javax.swing.plaf.PanelUI;
-
 /**
+ * 双向链表demo
+ *
  * @author mit
- * @date 2021/7/21 下午3:18
+ * @date 2021/7/22 上午11:52
  */
-public class SingleLinkedListDemo {
+public class DoubleLinkedListDemo {
     public static void main(String[] args) {
+        // 测试
+        System.out.println("双向链表的测试");
         // 先创建节点
-        HeroNode hero1 = new HeroNode(1, "宋江", "及时雨");
-        HeroNode hero2 = new HeroNode(2, "卢俊义", "玉麒麟");
-        HeroNode hero3 = new HeroNode(3, "吴用", "智多星");
-        HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
-        // 创建链表
-        SingleLinkedList singleLinkedList = new SingleLinkedList();
-        // singleLinkedList.add(hero1);
-        // singleLinkedList.add(hero4);
-        // singleLinkedList.add(hero2);
-        // singleLinkedList.add(hero3);
-
-        //加入按照编号的顺序
-        singleLinkedList.addByOrder(hero1);
-        singleLinkedList.addByOrder(hero4);
-        singleLinkedList.addByOrder(hero2);
-        singleLinkedList.addByOrder(hero3);
-
-        //显示一把
-        singleLinkedList.list();
-
-        //测试修改节点的代码
-        HeroNode newHeroNode = new HeroNode(2, "小卢", "玉麒麟~~");
-        singleLinkedList.update(newHeroNode);
-
-        System.out.println("修改后的链表情况~~");
-        singleLinkedList.list();
-
-        //删除一个节点
-        singleLinkedList.del(1);
-        singleLinkedList.del(4);
+        HeroNode2 hero1 = new HeroNode2(1, "宋江", "及时雨");
+        HeroNode2 hero2 = new HeroNode2(2, "卢俊义", "玉麒麟");
+        HeroNode2 hero3 = new HeroNode2(3, "吴用", "智多星");
+        HeroNode2 hero4 = new HeroNode2(4, "林冲", "豹子头");
+        // 创建一个双向链表
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+        doubleLinkedList.add(hero1);
+        doubleLinkedList.add(hero2);
+        doubleLinkedList.add(hero3);
+        doubleLinkedList.add(hero4);
+        doubleLinkedList.list();
+        // 修改
+        HeroNode2 newHeroNode = new HeroNode2(4, "公孙胜", "入云龙");
+        doubleLinkedList.update(newHeroNode);
+        System.out.println("修改后的链表情况");
+        doubleLinkedList.list();
+        // 删除
+        doubleLinkedList.del(3);
         System.out.println("删除后的链表情况~~");
-        singleLinkedList.list();
+        doubleLinkedList.list();
     }
-
 }
 
 /**
- * 单链表
+ * 双向链表
  *
  * @author mit
- * @date 2021/7/21 下午3:20
+ * @date 2021/7/22 上午11:53
  */
-class SingleLinkedList {
+class DoubleLinkedList {
+
     /**
      * 初始化一个头节点，头节点不存放具体的数据
      */
-    private final HeroNode head = new HeroNode(0, "", "");
+    private final HeroNode2 head = new HeroNode2(0, "", "");
+
+    public HeroNode2 getHead() {
+        return head;
+    }
 
     /**
-     * 添加节点到链表中-添加到链表尾部
+     * 显示链表(遍历)
+     *
+     * @author mit
+     * @date 2021/7/22 下午4:54
+     */
+    public void list() {
+        if (head.next == null) {
+            System.out.println("链表为空～～");
+            return;
+        }
+        // 遍历链表
+        HeroNode2 temp = head.next;
+        while (temp != null) {
+            System.out.printf("节点编号: %d, name: %s, nickName: %s \n", temp.no, temp.name, temp.nickName);
+            temp = temp.next;
+        }
+    }
+
+    /**
+     * 添加节点到双向链表中-添加到链表尾部
      *
      * @param heroNode 添加的节点
      * @author mit
      * @date 2021/7/21 下午3:31
      */
-    public void add(HeroNode heroNode) {
+    public void add(HeroNode2 heroNode) {
         // 查找到链表的最后一个节点，将最后节点的next指向新节点
-        HeroNode temp = head;
+        HeroNode2 temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
+        heroNode.pre = temp;
         temp.next = heroNode;
     }
 
@@ -80,8 +94,8 @@ class SingleLinkedList {
      * @author mit
      * @date 2021/7/21 下午3:42
      */
-    public void addByOrder(HeroNode heroNode) {
-        HeroNode temp = head;
+    public void addByOrder(HeroNode2 heroNode) {
+        HeroNode2 temp = head;
         boolean hasSameNo = false;
         while (true) {
             if (temp.next == null) {
@@ -99,8 +113,10 @@ class SingleLinkedList {
         if (hasSameNo) {
             System.out.printf("hero no %d already exists, can't add it \n", heroNode.no);
         } else {
+            heroNode.pre = temp;
             heroNode.next = temp.next;
             temp.next = heroNode;
+            temp.next.next.pre = heroNode;
         }
     }
 
@@ -111,25 +127,24 @@ class SingleLinkedList {
      * @author mit
      * @date 2021/7/21 下午3:53
      */
-    public void update(HeroNode newHeroNode) {
+    public void update(HeroNode2 newHeroNode) {
         // 链表可能存在为空的情况
         if (head.next == null) {
             System.out.println("链表为空～～");
             return;
         }
         // 找出no相同的节点
-        HeroNode temp = head.next;
+        HeroNode2 temp = head.next;
         boolean isFind = false;
         while (true) {
-            if (temp.next == null) {
+            if (temp== null) {
                 break;
             }
-            if (temp.no < newHeroNode.no) {
-                temp = temp.next;
-            } else if (temp.no == newHeroNode.no) {
+            if (temp.no == newHeroNode.no) {
                 isFind = true;
                 break;
             }
+            temp = temp.next;
         }
         if (isFind) {
             temp.name = newHeroNode.name;
@@ -152,61 +167,45 @@ class SingleLinkedList {
             return;
         }
         // 找出对应编号的节点
-        HeroNode temp = head;
+        HeroNode2 temp = head.next;
         boolean isFind = false;
         while (true) {
-            if (temp.next == null) {
+            if (temp == null) {
                 break;
             }
-            if (temp.next.no == no) {
+            if (temp.no == no) {
                 isFind = true;
                 break;
             }
             temp = temp.next;
         }
         if (isFind) {
-            temp.next = temp.next.next;
+            temp.pre.next = temp.next;
+            if (temp.next != null) {
+                temp.next.pre = temp.pre;
+            }
         } else {
             System.out.printf("没有找到编号为 %d 的节点！\n", no);
         }
     }
-
-    /**
-     * 显示链表(遍历)
-     *
-     * @author mit
-     * @date 2021/7/22 上午11:12
-     */
-    public void list() {
-        if (head.next == null) {
-            System.out.println("链表为空～～");
-            return;
-        }
-        // 遍历链表
-        HeroNode temp = head.next;
-        while (temp != null) {
-            System.out.printf("节点编号: %d, name: %s, nickName: %s \n", temp.no, temp.name, temp.nickName);
-            temp = temp.next;
-        }
-    }
-
-
 }
 
+
 /**
- * 定义 HeroNode, 每个 HeroNode 对象就是一个节点
+ * 定义双向链表使用的 HeroNode, 每个 HeroNode 对象就是一个节点
  *
  * @author mit
  * @date 2021/7/21 下午3:21
  */
-class HeroNode {
+class HeroNode2 {
 
     public int no;
     public String name;
     public String nickName;
-    public HeroNode next;
+    public HeroNode2 pre;
+    public HeroNode2 next;
 
-    public HeroNode(int no, String name, String nickName) {
+    public HeroNode2(int no, String name, String nickName) {
         this.no = no;
         this.name = name;
         this.nickName = nickName;
@@ -214,7 +213,7 @@ class HeroNode {
 
     @Override
     public String toString() {
-        return "HeroNode{" +
+        return "HeroNode2{" +
                 "no=" + no +
                 ", name='" + name + '\'' +
                 ", nickName='" + nickName + '\'' +
